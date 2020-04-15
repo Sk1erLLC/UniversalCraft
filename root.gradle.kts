a9b69caa881986cffbc57857a14da68828cc94af
Mitchell Katz <Sk1er@Sk1er.club>
2020-04-15 16:22:09 -0400

    fix fabric build script error

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.3.40" apply false
-//    id("fabric-loom") version "0.2.6-SNAPSHOT" apply false
+    id("fabric-loom") version "0.2.6-SNAPSHOT" apply false
     id("com.replaymod.preprocess") version "8385acb"
 }
 
